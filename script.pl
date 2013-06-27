fcdc460aabd191dbf4db6d2ccc7eaad0c680f4ef
Christian Garbs <mitch@cgarbs.de>
2013-06-27 20:28:17 +0200

    code duplication
    Both the sum calculation and the print() statements appear twice in the code.
    
    In most cases, this is a bad thing, because in bigger projects, the
    repetitions won't be directly next to each other.  When one part is
    changed or fixed, the other might be missed and the code behaves
    differently.
    
    By writing a subroutine for the code, it can be called from different
    locations with different parameters and is guaranteed to behave
    exactly the same.

--- script.pl
+++ script.pl
@@ -1,21 +1,19 @@
 #!/usr/bin/perl -w
 use strict;
 
-my @numbers = (1, 2, 3, 4);
+sub calculate_and_print_sum(@)
+{
+    my @numbers = @_;
 
-my $sum = 0;
-for (my $i = 0; $i < @numbers; $i++) {
-    $sum = $sum + $numbers[$i];
+    my $sum = 0;
+    for (my $i = 0; $i < @numbers; $i++) {
+	$sum = $sum + $numbers[$i];
+    }
+    print "The sum of the numbers [".join(",", @numbers)."] is: $sum\n";
 }
-print "The sum of the numbers [".join(",", @numbers)."] is: $sum\n";
 
+calculate_and_print_sum(1, 2, 3, 4);
 # add next number
-push @numbers, 5;
-
-$sum = 0;
-for (my $i = 0; $i < @numbers; $i++) {
-    $sum = $sum + $numbers[$i];
-}
-print "The sum of the numbers [".join(",", @numbers)."] is: $sum\n";
+calculate_and_print_sum(1, 2, 3, 4, 5);
 
 print "the end\n";
