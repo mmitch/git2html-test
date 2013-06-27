66b7e5620d177c69dcee1abcd390483426f1845b
Jane Doe <janedoe@example.com>
2013-06-27 19:52:08 +0200

    review pleez!
    this b my frist perl script, plz review
    
    thankzorz! <3 <3 <3

---
+++ script.pl
@@ -0,0 +1,21 @@
+#!/usr/bin/perl
+use strict;
+
+my @NUMBERS = (1, 2, 3, 4);
+
+my $sum = 0;
+for (my $i = 0; $i < @NUMBERS; $i++) {
+    $sum = $sum + @NUMBERS[$i];
+    }
+print "The sum of the numbers [".join(",", @NUMBERS)."] is: $sum\n";
+
+# add next number
+push @NUMBERS, 5;
+
+my $sum = 0;
+for (my $i = 0; $i < @NUMBERS; $i++) {
+    $sum = $sum + @NUMBERS[$i];
+    }
+print "The sum of the numbers [".join(",", @NUMBERS)."] is: $sum\n";
+
+print "the end\n";
