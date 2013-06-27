6f62f75e70b83a9613e7ae0cb75904213fca0473
Christian Garbs <mitch@cgarbs.de>
2013-06-27 19:57:58 +0200

    this needs no array
    If you really only want to sum up the first n numbers, you can do this
    in a for loop without needing an additional array.
    
    Be careful though:
    You can't count from 0 to n-1 like before, the for loop needs to go
    from 1 to n to give the correct result.
    
    For the print( join() ), the number list is created on the fly via the
    (1..n) list creator.

--- script.pl
+++ script.pl
@@ -1,21 +1,19 @@
 #!/usr/bin/perl
 use strict;
 
-my @NUMBERS = (1, 2, 3, 4);
-
+my $max = 4;
 my $sum = 0;
-for (my $i = 0; $i < @NUMBERS; $i++) {
-    $sum = $sum + @NUMBERS[$i];
+for (my $i = 1; $i <= $max; $i++) {
+    $sum = $sum + $i;
     }
-print "The sum of the numbers [".join(",", @NUMBERS)."] is: $sum\n";
+print "The sum of the numbers [".join(",", (1..$max))."] is: $sum\n";
 
 # add next number
-push @NUMBERS, 5;
-
+my $max = 5;
 my $sum = 0;
-for (my $i = 0; $i < @NUMBERS; $i++) {
-    $sum = $sum + @NUMBERS[$i];
+for (my $i = 1; $i <= $max; $i++) {
+    $sum = $sum + $i;
     }
-print "The sum of the numbers [".join(",", @NUMBERS)."] is: $sum\n";
+print "The sum of the numbers [".join(",", (1..$max))."] is: $sum\n";
 
 print "the end\n";
