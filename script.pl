5a6ed7af41cad3a2527c979b524a614bc5a91b1c
Christian Garbs <mitch@cgarbs.de>
2013-06-27 20:21:57 +0200

    always enable warnings
    (this is based on the already applied style-fixes)
    
    You should always enable warnings in your Perl scripts, either via the
    -w parameter on the first line or via "use warnings;".
    
    (Of cource, "use strict;" is as important as the warnings, but it's
     already set.  Good!)
    
    With -w, 3 warnings are printed:
    
    Scalar value @numbers[$i] better written as $numbers[$i] at script.pl line 8.
    "my" variable $sum masks earlier declaration in same scope at script.pl line 15.
    Scalar value @numbers[$i] better written as $numbers[$i] at script.pl line 17.
    
    These have been fixed also.

--- script.pl
+++ script.pl
@@ -1,20 +1,20 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
 use strict;
 
 my @numbers = (1, 2, 3, 4);
 
 my $sum = 0;
 for (my $i = 0; $i < @numbers; $i++) {
-    $sum = $sum + @numbers[$i];
+    $sum = $sum + $numbers[$i];
 }
 print "The sum of the numbers [".join(",", @numbers)."] is: $sum\n";
 
 # add next number
 push @numbers, 5;
 
-my $sum = 0;
+$sum = 0;
 for (my $i = 0; $i < @numbers; $i++) {
-    $sum = $sum + @numbers[$i];
+    $sum = $sum + $numbers[$i];
 }
 print "The sum of the numbers [".join(",", @numbers)."] is: $sum\n";
 
