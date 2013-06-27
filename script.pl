80f34fd42105e47e9dbddfba572ea4e3e7e11391
Christian Garbs <mitch@cgarbs.de>
2013-06-27 20:12:20 +0200

    this needs no for loop
    According to the Gaußsche Summenformel[1], the sum of the first n
    integers is
    
      n     n²+n
      ∑ k = ----
     k=1      2
    
    So by defining the formula f(x) = (x²+x)/2 the sum can be calculated
    directly and without the need for a loop.
    
    This is very fast for larger numbers as it is O(1) instead of O(n).
    
    [1] see http://de.wikipedia.org/wiki/Gau%C3%9Fsche_Summenformel

--- script.pl
+++ script.pl
@@ -1,19 +1,15 @@
 #!/usr/bin/perl
 use strict;
 
-my $max = 4;
-my $sum = 0;
-for (my $i = 1; $i <= $max; $i++) {
-    $sum = $sum + $i;
-    }
-print "The sum of the numbers [".join(",", (1..$max))."] is: $sum\n";
+sub f($)
+{
+    my $x = shift;
+    return ($x*$x + $x) / 2;
+}
+
+print "The sum of the numbers [".join(",", (1..4))."] is: ".f(4)."\n";
 
 # add next number
-my $max = 5;
-my $sum = 0;
-for (my $i = 1; $i <= $max; $i++) {
-    $sum = $sum + $i;
-    }
-print "The sum of the numbers [".join(",", (1..$max))."] is: $sum\n";
+print "The sum of the numbers [".join(",", (1..5))."] is: ".f(5)."\n";
 
 print "the end\n";
